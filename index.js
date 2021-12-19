93db8f7d4bffa02106e8eaaba92eb7749c2726c9
natoine <antoineseilles@gmail.com>
2021-12-19 18:15:50 +0100

    now use a html page for index

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Bienvenue sur punchliner - on bosse sur la home pour le moment !');
-  });
+app.use(express.static('resources/public'));
 
   app.listen(PORT, function () {
     console.log('Punchliner lancé sur le port :' + PORT);
