9b749ccbca0b453b433a086bb5488ed95292d3d6
natoine <antoineseilles@gmail.com>
2021-12-29 17:42:39 +0100

    now get and post punchlines

--- index.js
+++ index.js
@@ -7,13 +7,49 @@ const PORT = process.env.PORT || 3000;
 
 const dbservice = require('./services/DataBaseService');
 
+app.use(express.json());//needed to parse request body in json
+
 app.use(express.static('resources/public'));
 
-app.get("/punchline", function(request, response){
+app.get("/punchline", function (request, response) {
   var punchline = { "lyrics": "qui peut prétendre faire du rap sans prendre position", "song": "menace de mort", "punchliner": "youssoupha" };
   response.send(punchline);
 });
 
-  app.listen(PORT, function () {
-    console.log('Punchliner lancé sur le port :' + PORT);
+
+app.get('/punchlines', function (request, response) {
+  dbservice.getPunchlines(function (error, data) {
+    if (error) {
+      response.writeHead(error.code, {
+        'Content-Length': Buffer.byteLength(error.message),
+        'Content-Type': 'text/plain'
+      }).end(error.message);
+    }
+    else {
+      response.send(data);
+    }
   });
+});
+
+app.post('/newpunchline', function (request, response) {
+  const newpunchline = request.body;
+  dbservice.createPunchline(newpunchline, function (error, newPunchlineId) {
+    if (error) {
+      response.writeHead(error.code, {
+        'Content-Length': Buffer.byteLength(error.message),
+        'Content-Type': 'text/plain'
+      }).end(error.message);
+    }
+    else {
+      let resbody = "punchline created with id : " + newPunchlineId.toHexString();
+      response.writeHead(201, {
+        'Content-Length': Buffer.byteLength(resbody),
+        'Content-Type': 'text/plain'
+      }).end(resbody);
+    }
+  });
+});
+
+app.listen(PORT, function () {
+  console.log('Punchliner lancé sur le port :' + PORT);
+});
--- services/DataBaseService.js
+++ services/DataBaseService.js
@@ -1,10 +1,12 @@
 const mongodb = require('mongodb');
 const urimongo = require("../resources/secret/databaseconfig.js").url;
+const dbname = "punchliner" ;
+const punchlinescollection = "punchlines" ;
 //console.log(urimongo);
 
-function getLends(callback) {
+function getPunchlines(callback) {
     mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }).then(client => {
-        client.db("petitsemprunts").collection("lends").find().toArray().then(items => {
+        client.db(dbname).collection(punchlinescollection).find().toArray().then(items => {
             callback(null, items);
             client.close();
         });
@@ -14,64 +16,20 @@ function getLends(callback) {
         callback(error, null);});
 }
 
-function createLend(lend, callback) {
+function createPunchline(punchline, callback) {
     mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }).then(client => {
-        client.db("petitsemprunts").collection("lends").insertOne(lend).then(function(data){
+        client.db(dbname).collection(punchlinescollection).insertOne(punchline).then(function(data){
             callback(null, data.insertedId);
             client.close();
         });
     }).catch(function(err){
+        console.log("error", err);
         const error = new Error("unable to connect DB");
         error.code = 500 ;
         callback(error, null);});
 }
 
-function createUser(user, callback) {
-    mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }).then(client => {
-        console.log("test username");
-        client.db("petitsemprunts").collection("users").findOne({ "username": user.username }).then(function(data){
-            if(data)
-            {
-                const error = new Error("username already in use");
-                error.code = 406;
-                callback(error, null);
-                client.close();
-            }
-            else 
-            {
-                console.log("test usermail");
-                client.db("petitsemprunts").collection("users").findOne({ "usermail": user.usermail }).then(function(data){
-                    if(data) 
-                    {
-                        const error = new Error("mail already in use");
-                        error.code = 406;
-                        callback(error, null);
-                        client.close();
-                    }
-                    else
-                    {
-                        console.log("creates user");
-                        client.db("petitsemprunts").collection("users").insertOne(user).then(function(data){
-                            callback(null, data.insertedId);
-                            client.close();
-                        });
-                    }
-                });
-            }
-        });
-    }).catch(error => {
-        if(error.code) callback(error, null);
-        else 
-        {
-            const err = new Error("unable to connect DB");
-            error.code = 500 ;
-            callback(error, null);
-        }
-    });
-}
-
 module.exports = {
-    getLends: getLends,
-    createLend: createLend,
-    createUser: createUser
+    getPunchlines: getPunchlines,
+    createPunchline: createPunchline,
 }
